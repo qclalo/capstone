77b908346c0b9d021ff2b59216d2b79ee24a5d15
Alec Condry <aleccondry@gmail.com>
2022-09-25 12:54:49 -0400

    Basic app structure template

---
+++ NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/MainMenuFragment.kt
@@ -0,0 +1,44 @@
+package com.example.nemocode_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.nemocode_app.databinding.FragmentMainMenuBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MainMenuFragment : Fragment() {
+
+    private var _binding: FragmentMainMenuBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMainMenuBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
