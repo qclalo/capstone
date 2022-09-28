4e78061c12673534f2a85dcf34607eaf8c20b201
Alec Condry <aleccondry@gmail.com>
2022-09-28 13:28:35 -0400

    Improved argument passing functionality, added function to in InstructionMenuFragment to get all text edit box values and place them into a String array for sending to MainMenuFragment

--- NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/InstructionMenuFragment.kt
+++ NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/InstructionMenuFragment.kt
@@ -3,10 +3,13 @@ package com.example.nemocode_app
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewDebug
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputEditText
@@ -22,11 +25,8 @@ class InstructionMenuFragment : Fragment() {
 
         val addDeviceConfirmBtn : Button = view.findViewById(R.id.add_device_confirm)
         addDeviceConfirmBtn.setOnClickListener {
-            val userNameEditText : TextInputEditText = view.findViewById(R.id.userName)
-            val userName = userNameEditText.text.toString()
-
-            val action = InstructionMenuFragmentDirections.actionInstructionMenuFragmentToMainMenuFragment(userName)
-            
+            val deviceInfo = getDeviceInfo(view)
+            val action = InstructionMenuFragmentDirections.actionInstructionMenuFragmentToMainMenuFragment(deviceInfo)
             findNavController().navigate(action)
         }
 
@@ -34,5 +34,20 @@ class InstructionMenuFragment : Fragment() {
         cancelBtn.setOnClickListener {
             findNavController().navigate(R.id.action_instructionMenuFragment_to_mainMenuFragment)
         }
+    }
+
+    // Get the contents of all TextInputEditText boxes and return them in a string array
+    private fun getDeviceInfo(view: View) : Array<String> {
+        val vg : ViewGroup = view as ViewGroup
+        val deviceInfoList : MutableList<String> = MutableList(0) {i -> ""}
+        var cnt = 0
+        for (i in 0..vg.childCount) {
+            if (vg.getChildAt(i) is TextInputEditText) {
+                val currInputEditText : TextInputEditText = vg.getChildAt(i) as TextInputEditText
+                deviceInfoList.add(currInputEditText.text.toString())
+                cnt += 1
+            }
+        }
+        return deviceInfoList.toTypedArray()
     }
 }
--- NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/MainMenuFragment.kt
+++ NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/MainMenuFragment.kt
@@ -14,14 +14,19 @@ class MainMenuFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main_menu, container, false)
         return view
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val noDevicesTextView: TextView = view.findViewById(R.id.no_devices)
-        noDevicesTextView.text = arguments.toString()
-        
+        if (arguments?.get("deviceInfo") != null) {
+            //noDevicesTextView.visibility = View.INVISIBLE
+            noDevicesTextView.text = (arguments?.get("deviceInfo") as Array<*>)[0].toString()
+        }
+
         val addDeviceBtn : Button = view.findViewById(R.id.add_device_main)
         addDeviceBtn.setOnClickListener {
             val action = MainMenuFragmentDirections.actionMainMenuFragmentToInstructionMenuFragment()
