7996e065ca866e8ba722be0aa311ce200bf5f2cc
Alec Condry <aleccondry@gmail.com>
2022-09-27 22:39:58 -0400

    new project base template

--- NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/MainMenuFragment.kt
+++ NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/MainMenuFragment.kt
@@ -1,44 +1,25 @@
 package com.example.nemocode_app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.nemocode_app.databinding.FragmentMainMenuBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class MainMenuFragment : Fragment() {
-
-    private var _binding: FragmentMainMenuBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentMainMenuBinding.inflate(inflater, container, false)
-        return binding.root
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_main_menu, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        val addDeviceBtn : Button = view.findViewById(R.id.add_device_main)
+        addDeviceBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenuFragment_to_instructionMenuFragment)
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
