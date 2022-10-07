ca031749276882646ff9bd1d697715ab622681ec
Derek Ehle <dehle98@gmail.com>
2022-10-06 21:21:57 -0400

    Removes interface for Controller and moves comments from interface into discrete class.

--- NEMoCoDe/DataStructures.py
+++ NEMoCoDe/DataStructures.py
@@ -42,102 +42,7 @@ class ImpactData:
         self.data = data
 
 
-class ControllerInterface:
-    @abstractmethod
-    def get_accelerometer_packet(self, id: str) -> AccelerometerPacket:
-        """
-        Gets a packet from an accelerometer with a given id
-        :param str id: The id of the accelerometer to pull data from
-        returns A packet representing x - y - z acceleration at a moment in time
-        """
-        pass
-
-    @abstractmethod
-    def assemble_package(self, packets) -> Package:
-        """
-        :param Packet[] packets: a group of packets representing a single data collection moment
-        :raises An error if the time between packets is not adequately synchronized or ids are different
-        :returns A Package created from an array of Packets with synchronized timings
-        """
-        pass
-
-    @abstractmethod
-    def add_package_to_queue(self, pack: Package):
-        """
-        Adds a Package to the front of the queue and removes the oldest Package from the queue
-        :param Package pack: the Package object to be added to the queue
-        """
-        pass
-
-    @abstractmethod
-    def initialize_connection(self, accel_port: int, id: str) -> AccelerometerPacket:
-        """
-        :param SerialID accel_port: serial id of the specific accelerometer being connected to
-        :param str id: ID to set for the accelerometer
-        :raises An error if the connection has failed to initialize
-        :returns an accelerometer object representing the accelerometer connected to
-        """
-        pass
-
-    @abstractmethod
-    def run_data_collection_loop(self):
-        """
-        Start collecting data from all of the accelerometers
-        Save this data in a circular array / queue type data struct
-        Watch for potentially concussive events
-        If a concussive event is detected, take a snapshot of the data and send back to user
-        """
-        pass
-
-    @abstractmethod
-    def check_package_for_concussion(self, package: Package) -> bool:
-        """
-        Algorithmic checking of queue for potential concussive impact above threshold limit
-        :returns true or false indicating if a concussive event has occurred
-        """
-        pass
-
-    @abstractmethod
-    def create_impact_data(self) -> ImpactData:
-        """
-        Create an ImpactData object from the current queue data
-        This object will contain critical information that the client controller needs to visualize the data
-        """
-        pass
-
-    @abstractmethod
-    def alert_user(self, report: ImpactData):
-        """
-        Send alert to application with ImpactData for further analysis
-        :param ImpactData report: ImpactData object sent to application for further analysis
-        """
-        pass
-
-    @abstractmethod
-    def connect_to_user_device(self) -> bool:
-        """
-        connects the microcontroller to phone application and initiates mic`rocontroller in standby mode
-        @returns a true or false indicating if the connection is successful`
-        """
-        pass
-
-    @abstractmethod
-    def start_session(self):
-        """
-        Start data collection loop and give diagnostic info to microcontroller/application
-        """
-        pass
-
-    @abstractmethod
-    def run_standby_loop(self):
-        """
-        Monitor connection with accelerometers for functionality and connection with application for
-        start session indication.
-        """
-        pass
-
-
-class Controller(ControllerInterface):
+class Controller:
 
     def __init__(self, time: int, queue, queueIndex: int, queueLen: int, accel_ports):
         self.time = 0
@@ -147,6 +52,11 @@ class Controller(ControllerInterface):
         self.accel_ports = []
 
     def get_accelerometer_packet(self, id: int) -> AccelerometerPacket:
+        """
+        Gets a packet from an accelerometer with a given id
+        :param str id: The id of the accelerometer to pull data from
+        returns A packet representing x - y - z acceleration at a moment in time
+        """
         pass
 
     def assemble_package(self, packets) -> Package:
@@ -158,6 +68,10 @@ class Controller(ControllerInterface):
         return Package(self.time, packets)
 
     def add_package_to_queue(self, pack: Package):
+        """
+        Adds a Package to the front of the queue and removes the oldest Package from the queue
+        :param Package pack: the Package object to be added to the queue
+        """
         self.queue[self.queueIndex] = pack
         if(self.queueIndex == self.queueLen - 1):
             self.queueIndex = 0
@@ -165,9 +79,21 @@ class Controller(ControllerInterface):
             self.queueIndex = self.queueIndex + 1
 
     def initialize_connection(self, accel_port: int, id: int) -> AccelerometerPacket:
+        """
+        :param SerialID accel_port: serial id of the specific accelerometer being connected to
+        :param str id: ID to set for the accelerometer
+        :raises An error if the connection has failed to initialize
+        :returns an accelerometer object representing the accelerometer connected to
+        """
         pass
 
     def run_data_collection_loop(self):
+        """
+        Start collecting data from all of the accelerometers
+        Save this data in a circular array / queue type data struct
+        Watch for potentially concussive events
+        If a concussive event is detected, take a snapshot of the data and send back to user
+        """
         packets = []
         for index in self.accel_ports:
             packet = self.get_accelerometer_packet(index)
@@ -179,6 +105,10 @@ class Controller(ControllerInterface):
             self.alert_user(data)
 
     def check_package_for_concussion(self, package: Package) -> bool:
+        """
+        Algorithmic checking of queue for potential concussive impact above threshold limit
+        :returns true or false indicating if a concussive event has occurred
+        """
         for pack in package.packs:
             for val in pack.get_accel_data():
                 if (val >= ACCEL_THRESHOLD):
@@ -186,16 +116,35 @@ class Controller(ControllerInterface):
         return False
 
     def create_impact_data(self) -> ImpactData:
+        """
+        Create an ImpactData object from the current queue data
+        This object will contain critical information that the client controller needs to visualize the data
+        """
         pass
 
     def alert_user(self, report: ImpactData):
+        """
+        Send alert to application with ImpactData for further analysis
+        :param ImpactData report: ImpactData object sent to application for further analysis
+        """
         pass
 
     def connect_to_user_device(self) -> bool:
+        """
+        connects the microcontroller to phone application and initiates mic`rocontroller in standby mode
+        @returns a true or false indicating if the connection is successful`
+        """
         pass
 
     def start_session(self):
+        """
+        Start data collection loop and give diagnostic info to microcontroller/application
+        """
         pass
 
     def run_standby_loop(self):
+        """
+        Monitor connection with accelerometers for functionality and connection with application for
+        start session indication.
+        """
         pass
