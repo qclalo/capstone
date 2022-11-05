56953c97dfd9f5910c46acda4df53cffe46d6284
arbidyusuf <arbidyusuf@gmail.com>
2022-11-04 22:13:27 -0400

    Added bluetooth adapter

--- NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/InstructionMenuFragment.kt
+++ NEMoCoDe_App/app/src/main/java/com/example/nemocode_app/InstructionMenuFragment.kt
@@ -74,4 +74,16 @@ class InstructionMenuFragment : Fragment() {
         }
         return true
     }
+    
+    // BLUETOOTH:
+    val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+    val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+    if (bluetoothAdapter == null) {
+        // Device doesn't support Bluetooth
+    }
+
+    if (bluetoothAdapter?.isEnabled == false) {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+    }
 }
